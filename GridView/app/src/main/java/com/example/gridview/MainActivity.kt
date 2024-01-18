66f61b4e1eae6a4276bda6b8eecd8a982d39a56f
c0delust <saadmulla2077@gmail.com>
2024-01-18 15:51:15 +0530

    Update

--- GridView/app/src/main/java/com/example/gridview/MainActivity.kt
+++ GridView/app/src/main/java/com/example/gridview/MainActivity.kt
@@ -1,27 +1,40 @@
-import android.os.Bundle
-import android.widget.AdapterView.OnItemClickListener
-import android.widget.GridView
-import android.widget.Toast
+package com.example.gridview
+
+import MainAdapter
 import androidx.appcompat.app.AppCompatActivity
-import com.example.gridview.R
+import android.os.Bundle
+import android.widget.GridView
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var gridView: GridView
     private var playerNames = arrayOf(
-        "Cristiano Ronaldo",
-        "Joao Felix",
-        "Bernado Silva",
-        "Andre    Silve",
-        "BrunoFernandez",
-        "William Carvalho",
-        "Nelson Semedo",
-        "Pepe",
-        "Rui Patricio"
+        "\uD83C\uDDE6\uD83C\uDDE9\n Andorra",
+        "\uD83C\uDDE6\uD83C\uDDEA\n United Arab Emirates ",
+        "\uD83C\uDDE6\uD83C\uDDEB\n Afghanistan",
+        "\uD83C\uDDE6\uD83C\uDDEC\n Antigua and Barbuda ",
+        "\uD83C\uDDE6\uD83C\uDDEE\n Anguilla",
+        "\uD83C\uDDE6\uD83C\uDDF2\n Armenia",
+        "\uD83C\uDDE6\uD83C\uDDF4\n Angola",
+        "\uD83C\uDDE6\uD83C\uDDF6\n Antarctica",
+        "\uD83C\uDDE6\uD83C\uDDF7\n Argentina",
+        "\uD83C\uDDE6\uD83C\uDDF8\n American Samoa",
+        "\uD83C\uDDE6\uD83C\uDDF9\n Austria",
+        "\uD83C\uDDE6\uD83C\uDDFA\n Australia ",
+        "\uD83C\uDDE6\uD83C\uDDFC\n Aruba ",
+        "\uD83C\uDDE6\uD83C\uDDFD\n Åland Islands ",
+        "\uD83C\uDDE6\uD83C\uDDFF\n Azerbaijan ",
+        "\uD83C\uDDE7\uD83C\uDDE6\n Bosnia and Herzegovina",
+        "\uD83C\uDDE7\uD83C\uDDE7\n Barbados",
+        "\uD83C\uDDE7\uD83C\uDDE9\n Bangladesh",
+        "\uD83C\uDDE7\uD83C\uDDEA\n Belgium",
+        "\uD83C\uDDE7\uD83C\uDDEC\n Bulgaria"
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         title = "GridView"
         gridView = findViewById(R.id.gridView)
 
--- GridView/app/src/main/java/com/example/gridview/MainAdapter.kt
+++ GridView/app/src/main/java/com/example/gridview/MainAdapter.kt
@@ -1,36 +1,50 @@
-package com.example.gridview
-
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
+import com.example.gridview.R
 
-class TextAdapter(private var context: Context) : BaseAdapter() {
-    var layoutInflater: LayoutInflater? = null
+class MainAdapter(
+    private val context: Context,
+    private val numbersInWords: Array<String>,
+) :
+    BaseAdapter() {
+    private var layoutInflater: LayoutInflater? = null
+    private lateinit var textView: TextView
     override fun getCount(): Int {
-        return 60
+        return numbersInWords.size
     }
-
     override fun getItem(position: Int): Any? {
         return null
     }
-
     override fun getItemId(position: Int): Long {
         return 0
     }
-
-    override fun getView(i: Int, view: View, viewGroup: ViewGroup): View {
-        var view = view
-        if (layoutInflater == null) layoutInflater =
-            context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val textView = view.findViewById<TextView>(R.id.textView)
-        textView.text = "${i + 1}"
-        textView.setOnClickListener {
-            Toast.makeText(context, textView.text, Toast.LENGTH_SHORT).show()
+    override fun getView(
+        position: Int,
+        convertView: View?,
+        parent: ViewGroup
+    ): View? {
+        var convertView = convertView
+        if (layoutInflater == null) {
+            layoutInflater =
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         }
-        return view
+        if (convertView == null) {
+            convertView = layoutInflater!!.inflate(R.layout.view, null)
+        }
+
+        if (convertView != null) {
+            print("here if")
+            textView = convertView.findViewById(R.id.textView)
+            textView.text = numbersInWords[position]
+        } else {
+            print("here else")
+        }
+
+        return convertView
     }
 }
